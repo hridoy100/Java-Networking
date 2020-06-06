59677d20a9b4a0e3c2c5cfc8c32c76316410ff33
hridoy100 <raihanul.hridoy100@gmail.com>
2020-06-06 13:19:27 +0600

    Synchronization.java added new version

--- Threading/src/io/github/hridoy100/Synchronization.java
+++ Threading/src/io/github/hridoy100/Synchronization.java
@@ -1,45 +1,45 @@
 package io.github.hridoy100;
 
-class BarberShop {
-    void getHairCut(String style){
-        System.out.print("Started cutting [ " + style);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+class Printer {
+    void printAssignment(Person p){
+        System.out.println(p.name+"-->Printing started...");
+        for(int i=0; i<p.pages; i++){
+            System.out.println(p.name + "--> printed page#"+(i+1));
         }
-        System.out.println(" ]");
+        System.out.println(p.name+"-->Completed...");
     }
 }
-
 
 class Person implements Runnable {
 
-    String style;
-    final BarberShop shop;
+    String name;
+    final Printer printer;
+    int pages;
     Thread t;
 
-    Person(BarberShop shop, String style){
-        this.shop = shop;
-        this.style = style;
-        t = new Thread(this);
+    Person(String name, Printer p, int pages){
+        this.name= name;
+        this.printer = p;
+        this.pages = pages;
+        t= new Thread(this, name);
         t.start();
     }
 
+
     @Override
     public void run() {
-        synchronized (shop) {
-            shop.getHairCut(style);
+        synchronized (printer) {
+            printer.printAssignment(this);
         }
     }
 }
 
-
 public class Synchronization {
     public static void main(String[] args) {
-        BarberShop shop = new BarberShop();
-        Person person1 = new Person(shop, "Style1");
-        Person person2 = new Person(shop, "Style2");
-        Person person3 = new Person(shop, "Style3");
+        Printer printer = new Printer();
+        Person hridoy = new Person("hridoy", printer, 5);
+        Person mahim = new Person("mahim", printer, 10);
+        Person rayhan = new Person("rayhan", printer, 8);
     }
 }
